5e33ad6fbd8cb8bf4761634b52e158c77ef05e84
Nico <riosc.nicolas@gmail.com>
2016-08-18 23:17:34 -0300

    Se agrega un ejemplo piola de lo que se puede hacer en Angular

--- Prueba Angular/js/controller.js
+++ Prueba Angular/js/controller.js
@@ -1,4 +1,10 @@
 var app=angular.module("PrimerApp",[]);
 app.controller("PrimerController",function($scope){
     $scope.nombre = "Nico";
+    $scope.nuevoUsuario = {};
+    $scope.usuarios = [];
+    $scope.agregarUsuario = function(){
+        $scope.usuarios.push($scope.nuevoUsuario);
+        $scope.nuevoUsuario = {};
+    }
 });
