794d23d5e3330bddb5c53a5d5c8ca475da01880d
Facu <RossiFacundo94@gmail.com>
2016-09-05 17:51:55 -0300

    Actualizacion Pedidos pendientes

--- Codigo/js/pedidosPendientes-controller.js
+++ Codigo/js/pedidosPendientes-controller.js
@@ -1,10 +1,11 @@
-angular.module('cardDemo1', ['ngMaterial'])
-.controller('AppCtrl', function($scope) {
-  $scope.imagePath = 'img/washedout.png';
-})
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
-  $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
-  $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
-  $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+angular.module("pedidosPendientes", [])
+.controller('pedidosPendientesController', function($scope) {
+  $scope.pedidos = [
+  	{numero: 1, items :[{pedido: "Pollo al Horno con doradas",cantidad:2 }]},
+  	{numero: 2, items :[{pedido: "Tarta de Pollo",cantidad:1}]},
+  	{numero: 3, items :[{pedido: "Lomo Completo",cantidad:3}]},
+  	{numero: 4, items :[{pedido: "Muzza" , cantidad:2}]},
+  	{numero: 5, items :[{pedido: "Lomo Simple",cantidad:1}]},
+  	{numero: 6, items :[{pedido: "Empanadas de Atun",cantidad:2}]}
+  ];
 });
